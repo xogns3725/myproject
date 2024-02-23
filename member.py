414ec9e8d262ce3314f0c7fb43136d8fd25e888d
xogns3725 <xogns3725@naver.com>
2024-02-23 12:21:40 +0900

    코드 실행 부분 main 함수로 묶음

--- member.py
+++ member.py
@@ -23,47 +23,47 @@ class Post:
     def __str__(self):
         return f'게시글 제목: {self.title} 게시글 내용: {self.content}'
 
+class Main():
+    # ----- 코드 실행 ------
+    # 회원, 게시글 리스트 생성
+    members = []
+    posts = []
 
-# ----- 코드 실행 ------
-# 회원, 게시글 리스트 생성
-members = []
-posts = []
-
-# 회원 입력
-while True:
-    member = Member(*input('이름, 아이디, 비밀번호를 입력하시오: ').split())
-    members.append(member)
-    mem_add = input('회원을 추가하시겠습니까? (y/n): ').lower()
-    if mem_add == 'y':
-        continue
-    elif mem_add == 'n':
-        break
-
-# 게시글 작성
-for member in members:
-    member.display()
-
+    # 회원 입력
     while True:
-        post = Post(*input('게시글 제목, 내용을 입력하시오: ').split(), author=member)
-        posts.append(post)
-        post_add = input('글을 더 작성하시겠습니까? (y/n): ').lower()
-        if post_add == 'y':
+        member = Member(*input('이름, 아이디, 비밀번호를 입력하시오: ').split())
+        members.append(member)
+        mem_add = input('회원을 추가하시겠습니까? (y/n): ').lower()
+        if mem_add == 'y':
             continue
-        elif post_add == 'n':
+        elif mem_add == 'n':
             break
 
-# 특정 검색
-while True:
-    choice = int(input('작성자 검색은 1, 특정 단어 검색은 2, 종료하시려면 3을 눌러주세요: '))
-    if choice == 1:
-        parti_author = input('작성자의 아이디를 입력하시오: ')
-        for post in posts:
-            if post.author == parti_author:
-                print(post)
-    elif choice == 2:
-        find_content = input('검색할 내용을 입력하시오: ')
-        for post in posts:
-            if find_content in post.content:
-                print(post)
-    elif choice == 3:
-        break
+    # 게시글 작성
+    for member in members:
+        member.display()
+
+        while True:
+            post = Post(*input('게시글 제목, 내용을 입력하시오: ').split(), author=member)
+            posts.append(post)
+            post_add = input('글을 더 작성하시겠습니까? (y/n): ').lower()
+            if post_add == 'y':
+                continue
+            elif post_add == 'n':
+                break
+
+    # 특정 검색
+    while True:
+        choice = int(input('작성자 검색은 1, 특정 단어 검색은 2, 종료하시려면 3을 눌러주세요: '))
+        if choice == 1:
+            parti_author = input('작성자의 아이디를 입력하시오: ')
+            for post in posts:
+                if post.author == parti_author:
+                    print(post)
+        elif choice == 2:
+            find_content = input('검색할 내용을 입력하시오: ')
+            for post in posts:
+                if find_content in post.content:
+                    print(post)
+        elif choice == 3:
+            break
